e93af665459bb189204568a1f07b5b7eeff820bd
Zachary <me@zachary.lol>
2022-05-08 20:30:52 +1000

    Indenting fix

--- src/server.ts
+++ src/server.ts
@@ -10,29 +10,29 @@ const nextApp = next({ dev: config.devMode });
 const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(async() => {
-    const app: Express = express();
-    const server: http.Server = http.createServer(app);
-    const io: socketio.Server = new socketio.Server();
-    io.attach(server);
+  const app: Express = express();
+  const server: http.Server = http.createServer(app);
+  const io: socketio.Server = new socketio.Server();
+  io.attach(server);
 
-    /* # Endpoints for torrent and subtitles will be here 
-    app.get('/hello', async (_: Request, res: Response) => {
-        res.send('Hello World');
-    });
-    */
+  /* # Endpoints for torrent and subtitles will be here 
+  app.get('/hello', async (_: Request, res: Response) => {
+      res.send('Hello World');
+  });
+  */
 
-    io.on('connection', (socket: socketio.Socket) => {
-        console.log('connection');
-        socket.emit('status', 'Hello from Socket.io');
+  io.on('connection', (socket: socketio.Socket) => {
+    console.log('connection');
+    socket.emit('status', 'Hello from Socket.io');
 
-        socket.on('disconnect', () => {
-            console.log('client disconnected');
-        })
-    });
+    socket.on('disconnect', () => {
+      console.log('client disconnected');
+    })
+  });
 
-    app.all('*', (req: any, res: any) => nextHandler(req, res));
+  app.all('*', (req: any, res: any) => nextHandler(req, res));
 
-    server.listen(port, () => {
-        console.log(`> Ready on http://localhost:${port}`);
-    });
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
