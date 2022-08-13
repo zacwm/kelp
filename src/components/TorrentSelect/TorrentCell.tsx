db1414b29778d7c0bbdd0d605fb1eb1905bce26f
Michael <me@miikey.pw>
2022-08-13 13:11:09 +1000

    feat: a virtual list grid responsible for infinitely rendering torrent results

---
+++ src/components/TorrentSelect/TorrentCell.tsx
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from "react";
+
+import { Box, Text, Paper, Group, Transition, Loader, Image, createStyles } from "@mantine/core";
+
+interface Props {
+    title: any;
+    delayIndex: number;
+    onSelect(): void;
+}
+
+interface ImageProps {
+    src: string;
+    alt: string;
+}
+
+const useClasses = createStyles((theme) => ({
+    image: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: theme.colors.dark[8],
+    },
+    loading: {
+        position: "absolute",
+        zIndex: 2,
+    },
+    error: {
+        color: theme.colors.red[7],
+    }
+}))
+
+const PosterImage = React.memo((props: ImageProps) => {
+    const [isLoading, setLoading] = useState(true);
+    const [hasError, setError] = useState(!props.src);
+
+    const { cx, classes } = useClasses();
+
+    const onLoad = () => {
+        setLoading(false);
+    }
+
+    const onError = () => {
+        setError(true);
+        setLoading(false);
+    };
+
+    if (hasError) {
+        return (
+            <Paper className={cx(classes.image, classes.error)}>
+                <Text>Failed to load image poster</Text>
+            </Paper>
+        )
+    }
+
+    return (
+        <React.Fragment>
+            {
+                isLoading && (
+                    <Paper className={cx(classes.image, classes.loading)}
+                    >
+                        <Loader variant="bars" size="md"/>
+                    </Paper>
+                )
+            }
+            <Image 
+                src={props.src}
+                alt={props.alt}
+                onLoad={onLoad}
+                onError={onError} 
+            /> 
+        </React.Fragment>
+    )
+})
+
+const Torrent = (props: Props) => {
+    const [mounted, isMounted] = useState(false);
+
+    let timeout = null;
+
+    // I believe this causes a memory leak
+    // be careful when using this
+    useEffect(() => {
+        timeout = setTimeout(() => {
+            isMounted(true);
+        }, props.delayIndex * 5);
+
+        return () => {
+            clearTimeout(timeout);
+            isMounted(false);
+        }
+    }, [props.delayIndex])
+
+    return (
+        <Box
+            sx={{
+                position: "relative",
+                width: "100%",
+                height: "380px",
+            }}
+            title={props.title.title}
+        >
+            <Transition
+                mounted
+                duration={300}
+                transition="fade"
+            >
+                {(styles) => (
+                    <Paper 
+                        shadow="md"
+                        radius="sm"
+                        sx={{
+                            background: "#2C2E33",
+                            cursor: "pointer",
+                            userSelect: "none",
+                            overflow: "hidden",
+                            "&:hover": {
+                                background: "rgba(57, 59, 66)",
+                            },
+                            display: "flex",
+                            flexDirection: "column",
+                            height: "100% "
+                        }}
+                        onClick={props.onSelect}
+                        style={styles}
+                        >
+                            <Box sx={{
+                                height: "100%",
+                                position: "relative",
+                                overflow: "hidden",
+                            }}>
+                                <PosterImage 
+                                    src={props.title.images?.poster} 
+                                    alt={props.title.title}
+                                />
+                            </Box>
+                            <Box sx={{ padding: '4px 6px' }}>
+                                <Text weight={600} style={{
+                                    whiteSpace: "pre-wrap",
+                                }}>{ props.title.title }</Text>
+                                <Group spacing="xs">
+                                    <Text size={12}>{ props.title.year }</Text>
+                                    <Text size={12}>{ props.title.certification }</Text>
+                                </Group>
+                            </Box>
+                        </Paper>
+                )}
+            </Transition>
+        </Box>
+    )
+}
+
+
+const MemoizedTorrent = React.memo(Torrent, (prevProps: Props, nextProps: Props) => {
+    return prevProps.title === nextProps.title && prevProps.delayIndex === nextProps.delayIndex;
+});
+
+export default MemoizedTorrent;
---
+++ src/components/TorrentSelect/VirtualList.tsx
@@ -0,0 +1,133 @@
+import React, { useState, useRef, useMemo } from "react";
+import { VirtuosoGrid, GridComponents, GridItemContent } from "react-virtuoso";
+import { Box, Loader, createStyles, LoadingOverlay } from "@mantine/core";
+
+import MemoizedTorrent from "./TorrentCell";
+
+interface Props {
+    itemData: object[];
+    isLoading: boolean;
+    setSelectedTitle: React.Dispatch<React.SetStateAction<any>>;
+    fetchTorrentList: (page: number, concat: boolean, forceLoad: boolean, callback?: Function) => void;
+}
+
+const useStyles = createStyles((theme) => ({
+    list: {
+        display: "flex",
+        flexWrap: "wrap",
+    },
+    itemContainer: {
+        width: "20%",
+        display: "flex",
+        flex: "none",
+        padding: "10px",
+        alignContent: "stretch",
+        boxSizing: "border-box",
+        // [`@media (max-width: ${theme.breakpoints.lg}px)`]: {
+        //     width: "33%"
+        // },
+        // [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+        //     width: "50%"
+        // }
+    },
+}));
+
+const VirtualList = ({
+    itemData,
+    isLoading,
+    setSelectedTitle,
+    fetchTorrentList
+}: Props): React.ReactElement => {
+    const { classes } = useStyles();
+
+    const [shallowFetch, setShallowFetch] = useState(false);
+
+    const hasMoreData = () => {
+        return itemData.length >= 50;
+    }
+
+    const lastPage = useRef(1);
+    const hasMore = useRef(hasMoreData());
+
+    const onEndReached = () => {
+        if (shallowFetch || !hasMore.current) {
+            return;
+        };
+
+        setShallowFetch(true);
+        lastPage.current += 1;
+        fetchTorrentList(lastPage.current, true, false, () => {
+            setShallowFetch(false);
+        });
+    }
+
+    const itemContent: GridItemContent<number> = (index: number) => {
+        const torrent = itemData[index];
+
+        if (torrent) {
+            return (
+                <MemoizedTorrent 
+                    key={index}
+                    title={torrent}
+                    onSelect={() => setSelectedTitle(torrent)}
+                    delayIndex={index}
+                />
+            )
+        } else {
+            return <div style={{ height: 1 }}/>
+        }        
+    }
+
+    const Components: GridComponents = useMemo(() => {
+        const List = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>((props, ref) => (
+            <div {...props} ref={ref} className={classes.list}/>
+        ))
+
+        const ItemContainer = (props: any) => {
+            return <div {...props} className={classes.itemContainer}/>
+        }
+
+        return {
+            List: List,
+            Item: ItemContainer
+        }
+    }, [itemData, isLoading])
+
+    if (isLoading) {
+        return (
+            <Box sx={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Loader />
+            </Box>
+        )
+    }
+
+    return (
+        <div style={{
+            height: "100%",
+            width: "100%",
+            padding: 32
+        }}>
+            <LoadingOverlay visible={shallowFetch} />
+            <VirtuosoGrid
+                style={{ height: "100%" }}
+                totalCount={itemData.length}
+                overscan={150}
+                components={Components}
+                itemContent={itemContent}
+                endReached={onEndReached}
+            />
+        </div>
+    )
+}
+
+const MemoizedVirtualList = React.memo(VirtualList, (prevProps: Props, nextProps: Props) => {
+    return JSON.stringify(prevProps.itemData) === JSON.stringify(nextProps.itemData) && prevProps.isLoading === nextProps.isLoading;
+});
+
+export default MemoizedVirtualList;
